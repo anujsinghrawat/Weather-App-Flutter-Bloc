b222b790aff56b674103f898cce73cb8d6fab760
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2024-02-02 10:45:07 +0530

    init

---
+++ lib/consts.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String baseUrl = "api.openweathermap.org";
+  static String cityEndPoint = 'data/2.5/weather';
+  static String locationEndPoint = 'data/2.5/weather';
+  static const String apiKey = "a6cbb38c0b717d146e22394a96a1f3f6";
+}
